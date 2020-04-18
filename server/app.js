109f47bdaff6e0476983a54c6023de49333d38e7
Srijan Manandhar <srijan@pitstoprecharge.com>
2020-04-18 10:41:26 +1000

    Setup express server with graphql schema for books and authors query

---
+++ server/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const graphqlHTTP = require("express-graphql");
+const schema = require("./schema/schema");
+
+const app = express();
+
+// middlewares
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+);
+
+app.listen(5000, () => {
+  console.log("now listening for requests on port 5000.");
+});
